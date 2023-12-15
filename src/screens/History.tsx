35e0d36fda23275cbabc5324c5c6c2f6f7014c47
Israel Malagutti <israelmalagutti@gmail.com>
2023-12-15 13:05:02 -0300

    Screen - Render HistoryCard with SectionList in History

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,16 +1,52 @@
-import { useNavigation } from "@react-navigation/native";
+import { Heading, SectionList, Text, VStack } from "native-base";
+import { HistoryCard, ScreenHeader } from "@components/index";
 
-import { Center, Text, VStack } from "native-base";
-import { ScreenHeader } from "@components/index";
+const HISTORY = [
+  {
+    title: "15-12-2023",
+    data: [
+      { name: "Puxada frontal", group: "Back", time: "8:44" },
+      { name: "Remada unilateral", group: "Back", time: "8:32" },
+    ],
+  },
+  {
+    title: "14-12-2023",
+    data: [{ name: "Puxada frontal", group: "Back", time: "11:24" }],
+  },
+];
 
 export function History() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Exercise history" />
 
-      <Center>
-        <Text>Hello History</Text>
-      </Center>
+      <SectionList
+        contentContainerStyle={[
+          { padding: 32, gap: 16 },
+          HISTORY.length < 1 && { flex: 1, justifyContent: "center" },
+        ]}
+        sections={HISTORY}
+        keyExtractor={item => item.name}
+        renderSectionHeader={({ section }) => (
+          <Heading
+            color="gray.200"
+            fontSize="md"
+            fontFamily="heading"
+            fontWeight="bold"
+          >
+            {section.title}
+          </Heading>
+        )}
+        renderItem={({ item }) => (
+          <HistoryCard name={item.name} group={item.group} time={item.time} />
+        )}
+        ListEmptyComponent={
+          <Text textAlign="center" color="gray.100">
+            There is no exercise yet. {"\n"}
+            Let's work out today?
+          </Text>
+        }
+      />
     </VStack>
   );
 }
