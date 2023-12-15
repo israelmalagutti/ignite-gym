ec26a77e55e2e4fb916e80e1f474244083ecd061
Israel Malagutti <israelmalagutti@gmail.com>
2023-12-15 11:53:55 -0300

    Screen - List exercises with ExerciseCards

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -4,7 +4,7 @@ import { useNavigation } from "@react-navigation/native";
 import { AppNavigatorRoutesProps } from "@routes/app.routes";
 
 import { FlatList, HStack, Heading, Text, VStack } from "native-base";
-import { Group, HomeHeader } from "@components/index";
+import { ExerciseCard, Group, HomeHeader } from "@components/index";
 
 type MuscleGroup =
   | "abs"
@@ -16,6 +16,13 @@ type MuscleGroup =
   | "triceps"
   | "legs";
 
+export type Exercise = {
+  name: string;
+
+  sets: number;
+  reps: number;
+};
+
 const EXERCISE_TYPES: MuscleGroup[] = [
   "all",
   "abs",
@@ -25,6 +32,13 @@ const EXERCISE_TYPES: MuscleGroup[] = [
   "glutes",
   "triceps",
   "legs",
+];
+
+const EXERCISES: Exercise[] = [
+  { name: "Seated cable row", sets: 3, reps: 12 },
+  { name: "Dumbbell row", sets: 3, reps: 12 },
+  { name: "Inverted row", sets: 3, reps: 12 },
+  { name: "Barbbell landmine row", sets: 3, reps: 12 },
 ];
 
 export function Home() {
@@ -44,6 +58,7 @@ export function Home() {
         horizontal
         showsHorizontalScrollIndicator={false}
         my={10}
+        minH={12}
         maxH={12}
         _contentContainerStyle={{
           alignItems: "center",
@@ -55,20 +70,41 @@ export function Home() {
         renderItem={({ item }) => (
           <Group
             name={item}
-            isActive={activeGroup === item}
+            isActive={activeGroup.toLowerCase() === item.toLowerCase()}
             onPress={() => handleActiveMuscleGroup(item)}
           />
         )}
       />
 
-      <HStack justifyContent="space-between" px={8}>
-        <Heading color={"gray.200"} fontSize={"sm"}>
-          Exercises
-        </Heading>
-        <Text color={"gray.200"} fontSize={"sm"}>
-          4
-        </Text>
-      </HStack>
+      <VStack flex={1} style={{ gap: 12 }}>
+        <HStack justifyContent="space-between" px={8}>
+          <Heading color={"gray.200"} fontSize={"sm"}>
+            Exercises
+          </Heading>
+          <Text color={"gray.200"} fontSize={"sm"}>
+            {EXERCISES.length}
+          </Text>
+        </HStack>
+
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{
+            gap: 3,
+            px: 8,
+            pb: 12,
+          }}
+          data={EXERCISES}
+          keyExtractor={item => item.name}
+          renderItem={({ item: exercise }) => (
+            <ExerciseCard
+              name={exercise.name}
+              sets={exercise.sets}
+              reps={exercise.reps}
+              onPress={() => navigation.navigate("exercise", { exercise })}
+            />
+          )}
+        />
+      </VStack>
     </VStack>
   );
 }
