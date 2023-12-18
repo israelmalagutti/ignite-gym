bdc2a837b421e42006be0f603727257727bc3c39
Israel Malagutti <israelmalagutti@gmail.com>
2023-12-18 11:22:41 -0300

    Screen - Mock Profile

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,6 +1,8 @@
-import { Input as NBInput, IInputProps } from "native-base";
+import { Input as NBInput, IInputProps, useTheme } from "native-base";
 
 export function Input({ ...rest }: IInputProps) {
+  const { colors } = useTheme();
+
   return (
     <NBInput
       bg="gray.700"
@@ -16,6 +18,10 @@ export function Input({ ...rest }: IInputProps) {
       fontFamily="body"
       _focus={{
         borderColor: "green.500",
+      }}
+      _disabled={{
+        bg: `${colors.gray[600]}60`,
+        color: "gray.200",
       }}
       {...rest}
     />
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,16 +1,79 @@
-import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
 
-import { AppNavigatorRoutesProps } from "@routes/app.routes";
+import { Button, Input, ScreenHeader, UserPhoto } from "@components/index";
+import {
+  Center,
+  Heading,
+  ScrollView,
+  Skeleton,
+  Text,
+  VStack,
+} from "native-base";
+import { TouchableOpacity } from "react-native";
 
-import { Center, Text } from "native-base";
-// import {} from "@components/index";
-
-// import {} from "";
+const PHOTO_SIZE = 33;
 
 export function Profile() {
+  const [photoLoading, setPhotoLoading] = useState(true);
+
   return (
-    <Center>
-      <Text>Hello Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Profile" />
+
+      <ScrollView
+        flexGrow={1}
+        pt={6}
+        contentContainerStyle={{ gap: 32, paddingBottom: 24 }}
+      >
+        <Center style={{ gap: 8 }}>
+          {photoLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: "https://github.com/israelmalagutti.png" }}
+              size={PHOTO_SIZE}
+            />
+          )}
+
+          <TouchableOpacity>
+            <Text
+              color="green.500"
+              fontSize="md"
+              fontFamily="heading"
+              fontWeight="bold"
+            >
+              Update photo
+            </Text>
+          </TouchableOpacity>
+        </Center>
+
+        <VStack px={8} style={{ gap: 16 }}>
+          <Input placeholder="Name" />
+          <Input placeholder="E-mail" isDisabled value="example@email.com" />
+
+          <VStack pt={8} style={{ gap: 16 }}>
+            <Heading
+              mb={-2}
+              color="gray.200"
+              fontSize="md"
+              fontFamily="heading"
+              fontWeight="bold"
+            >
+              Change Password
+            </Heading>
+            <Input placeholder="New password" />
+            <Input placeholder="Old password" />
+          </VStack>
+
+          <Button title="Update" />
+        </VStack>
+      </ScrollView>
+    </VStack>
   );
 }
