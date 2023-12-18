20d2488fea8dca24ce485ff81bb023264cd3b68a
Israel Malagutti <israelmalagutti@gmail.com>
2023-12-18 16:12:52 -0300

    Screen - Implement expo image picker in Profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import * as ImagePicker from "expo-image-picker";
 
 import { Button, Input, ScreenHeader, UserPhoto } from "@components/index";
 import {
@@ -14,7 +15,32 @@ import { TouchableOpacity } from "react-native";
 const PHOTO_SIZE = 33;
 
 export function Profile() {
+  const [userPhoto, setUserPhoto] = useState(
+    "https://github.com/israelmalagutti.png"
+  );
   const [photoLoading, setPhotoLoading] = useState(true);
+
+  const handleUserPhotoSelect = async () => {
+    setPhotoLoading(true);
+
+    try {
+      const selectedPhoto = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if (selectedPhoto.canceled) return;
+
+      if (selectedPhoto.assets[0].uri)
+        setUserPhoto(selectedPhoto.assets[0].uri);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setPhotoLoading(false);
+    }
+  };
 
   return (
     <VStack flex={1}>
@@ -35,10 +61,7 @@ export function Profile() {
               endColor="gray.400"
             />
           ) : (
-            <UserPhoto
-              source={{ uri: "https://github.com/israelmalagutti.png" }}
-              size={PHOTO_SIZE}
-            />
+            <UserPhoto source={{ uri: userPhoto }} size={PHOTO_SIZE} />
           )}
 
           <TouchableOpacity>
@@ -47,6 +70,7 @@ export function Profile() {
               fontSize="md"
               fontFamily="heading"
               fontWeight="bold"
+              onPress={handleUserPhotoSelect}
             >
               Update photo
             </Text>
