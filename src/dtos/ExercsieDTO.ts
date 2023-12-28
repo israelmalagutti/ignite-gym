8cd763313490c3e3052757977e3c40a0bc364850
Israel Malagutti <israelmalagutti@gmail.com>
2023-12-28 14:36:44 -0300

    Type - Create ExerciseDTO

---
+++ src/dtos/ExercsieDTO.ts
@@ -0,0 +1,15 @@
+export type ExerciseDTO = {
+  id: string;
+
+  group: string;
+  name: string;
+
+  repetitions: number;
+  series: number;
+
+  demo: string;
+  thumb: string;
+
+  created_at: string;
+  updated_at: string;
+};
